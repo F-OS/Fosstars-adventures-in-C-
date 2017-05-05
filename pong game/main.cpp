77ac4c18959923e7597fdddf9c8eda7db868f7e7
F-OS <fosstarreagan@gmail.com>
2017-05-05 15:26:53 -0600

    Fixered V2, the fixering

--- pong game/main.cpp
+++ pong game/main.cpp
@@ -1,18 +1,10 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 using namespace std;
-SDL_Surface* buffer;
-SDL_Window* Window;
 SDL_Renderer* Render;
 SDL_Rect rectangle;
 SDL_Rect rectangle1;
-int positionx = 50;
-int positiony = 50;
-int sizex = 800;
-int sizey = 800;
-int cyan [4] = {0, 255, 255, 255};
-int grey [4] = {50, 50, 50, 100};
-bool loop = true;
+int dorender();
 int main()
 {
 	rectangle.x = 20;
@@ -23,60 +15,65 @@ int main()
 	rectangle1.y = 300;
 	rectangle1.w = 30;
 	rectangle1.h = 200;
+	int positionx = 50;
+	int positiony = 50;
+	int sizex = 800;
+	int sizey = 800;
+	SDL_Window* Window;
+	bool loop = true;
 	SDL_InitSubSystem(SDL_INIT_EVERYTHING);
 	Window = SDL_CreateWindow("Test", positionx, positiony, sizex, sizey, SDL_WINDOW_OPENGL);
 	Render = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_RenderSetLogicalSize(Render, sizex, sizey);
-	SDL_RenderClear(Render);
-	SDL_RenderFillRect(Render, &rectangle);
-	SDL_RenderFillRect(Render, &rectangle1);
-	cout << "RENDERED" << endl;
-	SDL_RenderPresent(Render);
+	dorender();
+
 	while(loop = true)
 		{
 			SDL_Event event;
+
 			while(SDL_PollEvent(&event))
 				{
 					if(event.type == SDL_QUIT)
 						loop = false;
-					else if(event.type == SDL_KEYDOWN)
-						{
-							switch(event.key.keysym.sym)
-								{
-									case SDLK_RIGHT:
-										rectangle1.y += 10;
-										SDL_RenderClear(Render);
-                                        SDL_RenderFillRect(Render, &rectangle1);
-                                        SDL_RenderFillRect(Render, &rectangle);
-										SDL_RenderPresent(Render);
-										break;
-									case SDLK_LEFT:
-										rectangle1.y -= 10;
-										SDL_RenderClear(Render);
-										SDL_RenderFillRect(Render, &rectangle1);
-										SDL_RenderFillRect(Render, &rectangle);
-										SDL_RenderPresent(Render);
-										break;
-									case SDLK_UP:
-										rectangle.y += 10;
-										SDL_RenderClear(Render);
-										SDL_RenderFillRect(Render, &rectangle);
-										SDL_RenderFillRect(Render, &rectangle1);
-										SDL_RenderPresent(Render);
-										break;
+					else
+						if(event.type == SDL_KEYDOWN)
+							{
+								switch(event.key.keysym.sym)
+									{
+										case SDLK_RIGHT:
+											rectangle1.y += 10;
+											dorender();
+											break;
 
-									case SDLK_DOWN:
-										rectangle.y -= 10;
-										SDL_RenderClear(Render);
-										SDL_RenderFillRect(Render, &rectangle);
-										SDL_RenderFillRect(Render, &rectangle1);
-										SDL_RenderPresent(Render);
-										break;
-								}
+										case SDLK_LEFT:
+											rectangle1.y -= 10;
+											dorender();
+											break;
 
-						}
+										case SDLK_UP:
+											rectangle.y -= 10;
+											dorender();
+											break;
+
+										case SDLK_DOWN:
+											rectangle.y += 10;
+											dorender();
+											break;
+									}
+
+							}
 				}
 
 		}
+
 	return 0;
+}
+
+int dorender()
+{
+	SDL_RenderClear(Render);
+	SDL_RenderFillRect(Render, &rectangle1);
+	SDL_RenderFillRect(Render, &rectangle);
+	SDL_RenderPresent(Render);
+
 }
